af6ce7f357d8dc16f6011d1b09717accf5f2aa24
David10U <127707318+David10U@users.noreply.github.com>
2023-04-16 19:53:29 -0600

    Subir Proyecto Actualizado

---
+++ ProyectStrucDatos-master/ProyectStrucDatos-master/ProyectoEstructurasDatos/src/main/java/Modulo3/Carrito.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modulo3;
+
+import Modulo1.Producto;
+import Modulo2.Cliente;
+
+/**
+ *
+ * @author durrozx
+ */
+public class Carrito 
+{
+    private Producto listaDeProducto;
+    private Cliente cliente;   
+
+    public Carrito(Producto listaDeProducto, Cliente cliente) 
+    {
+        this.listaDeProducto = listaDeProducto;
+        this.cliente = cliente;
+    }
+    
+    public void agregar()
+    {
+        
+    }
+    public void consultar()
+    {
+        
+    }
+}
